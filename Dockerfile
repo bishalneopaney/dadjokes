48dc5c03b705a5b87cf8ae4ab1aabe5cab65dca0
Bishal <bishalneopaney.dev@gmail.com>
2023-03-20 15:39:02 +0600

    genesis

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# FROM nginx:1.16.1-alpine
+FROM nginx:stable-alpine
+
+RUN apk upgrade --force
+
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+RUN rm -rf /www-data/*
+
+# TODO: Change 'folde-name' to directory one level below build files
+COPY ./script/* /www-data
+
+RUN echo "nginx -g 'daemon off;'" > run.sh
+
+ENTRYPOINT ["sh", "run.sh"]
---
+++ script/script.js
@@ -0,0 +1,29 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+
+
+generateJoke()
+
+
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+
+
+fetch("https://icanhazdadjoke.com/" , config)
+  .then((response) => response.json())
+  .then((data) => {
+    jokeEl.innerHTML = data.joke
+  })
+
+}
+
+
+
